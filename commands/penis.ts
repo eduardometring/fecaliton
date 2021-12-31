f95560aa0ec7d2dd7c49f767cb7e29ec97da7632
Eduardo Metring <eduardometring2@gmail.com>
2021-12-30 21:02:20 -0300

    Criando o comando de penis

---
+++ commands/penis.ts
@@ -0,0 +1,25 @@
+import { MessageEmbed } from "discord.js";
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'Diversão',
+    description: 'Veja o tamanho do pênis de seus amigos!',
+
+    callback: async ({message}) => {
+        const user = message.mentions.users.first()
+        if(!user) {
+        const embed = new MessageEmbed()
+        .setDescription(`**Seu pênis** \n8` + "=".repeat(Math.floor(Math.random() * 25)) + "D")
+        .setColor('RANDOM')
+        await message.reply ({
+            embeds: [embed]
+        })
+        } else { 
+        const embed = new MessageEmbed()
+        .setDescription(`**Pênis de ${user}** \n8` + "=".repeat(Math.floor(Math.random() * 25)) + "D")
+        .setColor('RANDOM')
+        await message.reply ({
+            embeds: [embed]
+        })
+    }}
+} as ICommand
