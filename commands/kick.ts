d728202252fa04d3ae62314aaa2012248e73093c
Eduardo Metring <eduardometring2@gmail.com>
2022-01-01 13:41:24 -0300

    Mudanças no comando kick

--- commands/kick.ts
+++ commands/kick.ts
@@ -1,3 +1,4 @@
+import { GuildMember } from "discord.js";
 import { ICommand } from "wokcommands";
 
 export default {
@@ -13,7 +14,20 @@ export default {
     expectedArgsTypes: ['USER', 'STRING'],
 
 
-    callback: async ({message, args}) => {
+    callback: async ({message, interaction, args}) => {
+        const target = message ? message.mentions.members?.first() : interaction.options.getMember('user') as GuildMember
+        if (!target) {
+            return 'Me diga quem você quer expulsar, porra!'
+        } 
+        if (!target.kickable) {
+            return 'Eu não consigo expulsar esse filho de uma prostituta'
+        }
 
+        args.shift()
+        const reason = args.join(' ')
+
+        target.kick(reason)
+
+        return `Você expulsou <@${target.id}>`
     }
 } as ICommand
