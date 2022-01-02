e875e53d94a4a8f3a275f17d8ecc871d38dfd087
Eduardo Metring <eduardometring2@gmail.com>
2022-01-02 17:50:04 -0300

    Criação do comando hug

---
+++ commands/hug.ts
@@ -0,0 +1,11 @@
+import { ICommand} from "wokcommands";
+
+export default {
+    category: 'Fun',
+    description: 'Use this to hug someone',
+    aliases: ['abraçar', 'h'],
+
+    callback: async ({message, args}) => {
+        
+    }
+}  as ICommand
