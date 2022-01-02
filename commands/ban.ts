d30d5f83ef6144dc3ed06b851b9397f503e4bad4
Eduardo Metring <eduardometring2@gmail.com>
2022-01-01 21:49:42 -0300

    Alterações no comando de ban

--- commands/ban.ts
+++ commands/ban.ts
@@ -3,6 +3,7 @@ import { ICommand } from "wokcommands";
 export default {
     category: "Moderação",
     description: "Banir membros do seu servidor",
+    aliases: ['banir'],
 
     callback: async ({message}) => {
 
