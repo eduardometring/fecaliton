20900777443fa7912a375219c3e0964fb095d986
Eduardo Metring <eduardometring2@gmail.com>
2022-01-03 17:38:01 -0300

    alterações no comando invite

---
+++ commands/invite.ts
@@ -0,0 +1,10 @@
+import { ICommand} from "wokcommands";
+
+export default {
+    category: 'Utility',
+    description: 'use this to invite fecaliton to your server',
+
+    callback: async ({message}) => {
+        
+    }
+} as ICommand
