999379145d34fe73b6501edc350b1e629963cd38
Eduardo Metring <eduardometring2@gmail.com>
2022-01-03 17:38:25 -0300

    alterações no comando stfu

---
+++ commands/stfu.ts
@@ -0,0 +1,10 @@
+import { ICommand} from "wokcommands";
+
+export default {
+    category: 'Fun',
+    description: 'just shut the fuck up',
+
+    callback: async ({message}) => {
+        
+    }
+} as ICommand
