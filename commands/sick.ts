a6a1ea38f52f5171009a8f40ab9be0ce3803062f
Eduardo Metring <eduardometring2@gmail.com>
2022-01-03 17:38:17 -0300

    alterações no comando sick

---
+++ commands/sick.ts
@@ -0,0 +1,10 @@
+import { ICommand} from "wokcommands";
+
+export default {
+    category: 'Fun',
+    description: `use this if u are sick`,
+
+    callback: async ({message}) => {
+        
+    }
+} as ICommand
