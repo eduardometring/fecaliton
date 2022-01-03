6966a432ec8586a207594520c86c494a472ce34f
Eduardo Metring <eduardometring2@gmail.com>
2022-01-03 17:37:47 -0300

    alterações no comando cry

---
+++ commands/cry.ts
@@ -0,0 +1,10 @@
+import { ICommand} from "wokcommands";
+
+export default {
+    category: 'Fun',
+    description: 'stop crying u are not alone',
+
+    callback: async ({message}) => {
+        
+    }
+} as ICommand
