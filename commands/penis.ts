0fe86361031ca894f156bea0cec92741528e97d6
Eduardo Metring <eduardometring2@gmail.com>
2022-01-04 22:13:49 -0300

    alterações no comando penis

--- commands/penis.ts
+++ commands/penis.ts
@@ -10,14 +10,14 @@ export default {
         const user = message.mentions.users.first() || args.join(" ")
         if(!user) {
         const embed = new MessageEmbed()
-        .setDescription(`**Seu pênis** \n8` + "=".repeat(Math.floor(Math.random() * 25)) + "D")
+        .setDescription(`**your penis** \n8` + "=".repeat(Math.floor(Math.random() * 25)) + "D")
         .setColor('RANDOM')
         await message.reply ({
             embeds: [embed]
         })
         } else { 
         const embed = new MessageEmbed()
-        .setDescription(`**Pênis de ${user}** \n8` + "=".repeat(Math.floor(Math.random() * 25)) + "D")
+        .setDescription(`**${user}'s penis** \n8` + "=".repeat(Math.floor(Math.random() * 25)) + "D")
         .setColor('RANDOM')
         await message.reply ({
             embeds: [embed]
