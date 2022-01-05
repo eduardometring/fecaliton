86374d4305f6271ef1f064263dbb430acd610ea9
Eduardo Metring <eduardometring2@gmail.com>
2022-01-04 22:14:04 -0300

    alterações no comando invite

--- commands/invite.ts
+++ commands/invite.ts
@@ -1,10 +1,17 @@
 import { ICommand} from "wokcommands";
+import { MessageEmbed } from "discord.js";
 
 export default {
     category: 'Utility',
     description: 'use this to invite fecaliton to your server',
 
     callback: async ({message}) => {
-        
+        const embed = new MessageEmbed()
+        .setColor('DARK_BLUE')
+        .setDescription('**teste👍**')
+
+        await message.reply({ 
+            embeds: [embed]
+        })
     }
 } as ICommand
