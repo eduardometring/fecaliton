e330f22572a76eaf1e2314e8546a2725ded01c78
Eduardo Metring <eduardometring2@gmail.com>
2022-01-05 23:07:39 -0300

    separando os comandos por categoria

---
+++ commands/kill.ts
@@ -0,0 +1,10 @@
+import { ICommand} from "wokcommands";
+
+export default {
+    category: 'Fun',
+    description: 'use it to kill someone',
+
+    callback: async ({message}) => {
+        
+    }
+} as ICommand
