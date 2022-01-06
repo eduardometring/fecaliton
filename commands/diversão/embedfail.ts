265644e32934e35cac6d964623abb553d8feb9cf
Eduardo Metring <eduardometring2@gmail.com>
2022-01-05 23:08:00 -0300

    criação da pasta diversão

---
+++ commands/diversão/embedfail.ts
@@ -0,0 +1,11 @@
+import { ICommand} from "wokcommands";
+
+export default {
+    category: 'Fun',
+    description: 'epic embed failure, laugh at this user',
+    aliases: ['embed', 'epicfailure', 'epicembedfail', 'fail', 'failure'],
+
+    callback: async ({message}) => {
+        
+    }
+} as ICommand
