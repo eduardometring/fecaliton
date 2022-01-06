9768ced47424f94abbcaba6341125247c298a53c
Eduardo Metring <eduardometring2@gmail.com>
2022-01-05 23:07:46 -0300

    criaçao da pasta uvnifj

---
+++ commands/diversão/cringe.ts
@@ -0,0 +1,10 @@
+import { ICommand} from "wokcommands";
+
+export default {
+    category: 'Fun',
+    description: 'ur cringe just stfu',
+
+    callback: async ({message}) => {
+        
+    }
+} as ICommand
