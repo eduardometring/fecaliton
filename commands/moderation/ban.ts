36743f73faaa8975f819d073bd2a01d5529f8232
Eduardo Metring <eduardometring2@gmail.com>
2022-01-09 19:57:20 -0300

    criação da pasta moderation

---
+++ commands/moderation/ban.ts
@@ -0,0 +1,11 @@
+import { ICommand } from "wokcommands";
+
+export default {
+    category: "Moderação",
+    description: "Banir membros do seu servidor",
+    aliases: ['banir'],
+
+    callback: async ({message}) => {
+
+    }
+} as ICommand
