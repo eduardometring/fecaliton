0e6d7e5fdc3d9edf5a87edf8873c9fbd19f6dd92
Eduardo Metring <eduardometring2@gmail.com>
2022-01-09 19:57:10 -0300

    alterações na pasta fun

---
+++ commands/diversão/tits.ts
@@ -0,0 +1,11 @@
+import { ICommand} from "wokcommands";
+
+export default {
+    category: 'Fun',
+    description: 'zyzz chest',
+    aliases: ['boobs', 'breasts'],
+
+    callback: async ({message}) => {
+        
+    }
+} as ICommand
