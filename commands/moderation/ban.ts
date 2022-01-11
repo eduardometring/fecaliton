e77fdb24160e37e8b62c7b67dd98366d9e5bfcfa
Eduardo Metring <eduardometring2@gmail.com>
2022-01-10 23:00:30 -0300

    Alterações no comando ban

--- commands/moderation/ban.ts
+++ commands/moderation/ban.ts
@@ -1,11 +1,34 @@
+import { GuildMember } from "discord.js";
 import { ICommand } from "wokcommands";
 
 export default {
-    category: "Moderação",
-    description: "Banir membros do seu servidor",
+    category: 'Moderação',
+    description: 'Banir membros do seu servidor',
     aliases: ['banir'],
 
-    callback: async ({message}) => {
+    permissions: ['KICK_MEMBERS'],
 
+    guildOnly: true,
+
+    minArgs: 2,
+    expectedArgs: "<user> <reason>",
+    expectedArgsTypes: ['USER', 'STRING'],
+
+
+    callback: async ({message, interaction, args}) => {
+        const target = message ? message.mentions.members?.first() : interaction.options.getMember('user') as GuildMember
+        if (!target) {
+            return 'Me diga quem você quer expulsar, porra!'
+        } 
+        if (!target.kickable) {
+            return 'Eu não consigo expulsar esse filho de uma prostituta'
+        }
+
+        args.shift()
+        const reason = args.join(' ')
+
+        target.kick(reason)
+
+        return `Você expulsou <@${target.id}>`
     }
 } as ICommand
